2385d37d203ace6297d1f2dbd7c88872771a422a
Shvn <shvnry11@gmail.com>
2017-04-29 12:53:46 +0530

    Added config and session

--- landslide/landslide/__init__.py
+++ landslide/landslide/__init__.py
@@ -6,3 +6,14 @@ from flask import Flask
 app = Flask(__name__)
 
 import landslide.views
+
+# Load the development configuration
+app.config.from_object('config.development')
+
+# Load the file specified by the APP_CONFIG_FILE environment variable
+# Variables defined here will override those in the default configuration
+app.config.from_envvar('APP_CONFIG_FILE', silent=True)
+
+# session
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
