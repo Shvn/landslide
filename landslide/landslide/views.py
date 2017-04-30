5d946a1e9f0361702fd2970d0f355603c5ab47b8
Krishanu <krishanu.das2009@gmail.com>
2017-04-30 13:29:54 +0530

    Fetch nasa data

--- landslide/landslide/views.py
+++ landslide/landslide/views.py
@@ -5,8 +5,19 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from landslide import app
+import urllib2
+import json
+
 
 @app.route('/')
 @app.route('/home')
 def home():
     return "hello"
+
+@app.route('/view-map')
+def viewmap():
+    req = urllib2.Request("https://data.nasa.gov/resource/tfkf-kniw.json")
+    opener = urllib2.build_opener()
+    f = opener.open(req)
+    jso = json.loads(f.read())
+    return jso
